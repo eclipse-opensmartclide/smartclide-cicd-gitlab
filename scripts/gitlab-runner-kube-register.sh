8b2243764945218ca71ccb8c8f1d13d333564a3f
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-24 16:54:13 +0200

    Added copyright headers
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- scripts/gitlab-runner-kube-register.sh
+++ scripts/gitlab-runner-kube-register.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#*******************************************************************************
+# Copyright (c) 2021 AIR Institute
+#
+# This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License 2.0
+# which accompanies this distribution, and is available at
+# https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#     Adrian Diarte Prieto - initial API and implementation
+#*******************************************************************************
 # This k8s script registers a gitlab runner to the given gitlab host. 
 
 # Load env variables
